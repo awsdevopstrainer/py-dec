92eb1e90c206c642b2b7439c5cf2daa4cb1787ea
awsdevopstrainer <69856631+awsdevopstrainer@users.noreply.github.com>
2025-12-16 06:59:08 +0530

    Add files via upload

---
+++ first-program.py
@@ -0,0 +1,73 @@
+# print("Hello folks! Welcome to python course")
+
+'''
+this python program is to print some outputs
+the above line is gonna print "Hello Folks! Welcome to python course"
+the below line is gonna print the number
+'''
+
+# print(123456)
+
+
+
+# Read the input from user
+
+# mywords = input("Please type your text here:")
+
+# print("you have typed below text")
+
+# print(mywords)
+
+# =======================================================================
+# Addition of two number
+# define two numbers
+# Add the numbers
+# print the output
+# =======================================================================
+
+# a = 10
+# b = 20
+
+# name = "santosh"
+
+# c = a+b
+
+# print(c)
+
+# print(type(a))
+# print(type(b))
+# print(type(c))
+# print(type(name))
+
+# =======================================================================
+# Addition of two number
+#  Get 2 inputs from the user
+# Add the inputs
+# print the output
+# =======================================================================
+
+# firstnumber = int(input("Enter the first number:"))
+
+# secondnumber = int(input("Enter the second number:"))
+
+# print(type(firstnumber))
+# print(type(secondnumber))
+
+# firstnumberint = int(firstnumber)
+# secondnumberint = int(secondnumber)
+
+# print(type(firstnumberint))
+# print(type(secondnumberint))
+
+# addition = firstnumber + secondnumber
+
+# print("Here is the output of addition:")
+# print(addition)
+
+# print(input("Enter first number:") + input("Enter the Second number:"))
+
+a = input("some values:")
+
+b = int(a)
+
+print(b)
---
+++ variables-operator.py
@@ -0,0 +1,125 @@
+
+# name = "Santosh"
+
+# print(type(name))
+
+# salary = 100000000000
+
+# print(type(salary))
+
+# name = 10
+
+# print(type(name))
+
+
+# a = 10
+# b = 10.5
+# c = 106+3j
+
+# print(type(a))
+# print(type(b))
+# print(type(c))
+
+
+# implicit
+
+# a = "10"
+
+# b = 10.5
+
+# c = a+b
+
+# print(c)
+
+# print(type(a))
+# print(type(b))
+# print(type(c))
+
+# explicit
+# x = int("10.687687")
+
+# a = x
+# b = 10.5
+# c = a+b
+
+# print(type(a))
+# print(type(b))
+# print(type(c))
+
+# b = 10.5
+
+# c = a+b
+
+# print(c)
+
+# ===================================================================
+
+# Operators
+
+# 1. Arithmetic operator
+# 2. Assignment operator
+# 3. Comparison Operator
+# 4. Special operators
+# 5. Bitwise operators
+# 6. Logical operators
+
+# + , - , * , / , // , % , **
+
+a = 10
+b = 3
+
+# c = a + b
+# d = a - b
+# e = a * b
+# f = a / b
+# g = a // b
+# h = a % b
+# i = a ** b
+# print(c,d,e,f,g,h,i)
+
+#  Assignment operator 
+# = , += , -= , *= , /= , %= , **=
+
+# a += 10
+# # a = a+10
+# print(a)
+
+# a -= 4
+# print(a)
+
+# a *= 5
+# print(a)
+
+# a /= 2
+# print(a)
+
+# a %= 5
+# print(a)
+
+# a = 55
+# a //= 5
+# print(a)
+
+# a **= 2
+
+# print(a)
+
+# Comparison operator
+# == , != , > , < , >= , <=
+# Return type of comparison operator is boolean
+
+print(a == b)
+print(a != b)
+print(a > b)
+print(a < b)
+print(a >= b)
+print(a <= b)
+
+
+# a = 4567
+
+# print()
+
+# b = 4535
+
+# c = 67546
