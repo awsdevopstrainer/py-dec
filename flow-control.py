fb16530bfa277f2ad20a597efa75f8f9ab9577de
awsdevopstrainer <69856631+awsdevopstrainer@users.noreply.github.com>
2025-12-17 07:54:38 +0530

    Update flow-control.py

--- flow-control.py
+++ flow-control.py
@@ -2,14 +2,107 @@
 # {
 #   statements
 # }
-a = 10
-b = 10
+# a = 10
+# b = 10
 
-if a > b :
-    print("This is to confirm a is greater than b")
-elif a < b:
-    print("This is to confirm a is less than b")
-elif a == b:
-    print("This is to confirm a is equals to b")
+# if a > b :
+#     print("This is to confirm a is greater than b")
+# elif a < b:
+#     print("This is to confirm a is less than b")
+# elif a == b:
+#     print("This is to confirm a is equals to b")
 
-print("---The program ends here---")
+# print("---The program ends here---")
+
+# Datatypes
+# Lists, Tuples, Set, Dict, Strings
+
+# list1 = [1,2,3,4,5]
+
+# list1_length= len(list1)
+
+# print(list1_length)
+# print(type(list1))
+
+# list2 = ["santosh","mani","deepti","venkat"]
+
+# list3 = [1,2,"jan","feb",2,34,5,6,"march"]
+
+# tuple1 = (1,2,3,4,5)
+# tuple2 = ("santosh","mani","deepti","venkat")
+
+# tuple3 = (1,2,3,"santosh","jan")
+
+# print(type(tuple1))
+# print(tuple3[2])
+
+# list4 = [1,2,3,4,5,6,7,6,5,4,3,2]
+# tuples4 = (1,2,3,4,5,6,7,6,5,4,3,2)
+# set1 = {1,2,3,4,5,6,7,6,5,4,3,2}
+
+# print(list4[3])
+# print(tuples4)
+# print(set1)
+
+# print(type(list4))
+# print(type(tuples4))
+# print(type(set1))
+
+# # Dict
+# employee1 = {
+#     "name": "santosh",
+#     "designation": "trainer",
+#     "country": "india",
+#     "department": "",
+#     "salary": 18423908908,
+#     "hobbies": ["swimming","travel"],
+#     "personal_details": {
+#         "fathers_name": "asdfklasjdfkl",
+#         "mothers_name": "adskfjaklsdjf",
+#         "siblings": ["adskjfkalsjdf","asdfasdf"]
+#     }
+# }
+
+# # print(employee1.keys())
+
+# employee1["state"] = "Pondicherry"
+# employee1["salary"] = 3242354234345345
+
+# print(employee1)
+# print(type(employee1))
+
+# list5 = [3,32,43,5]
+
+# print(list5)
+# print(type(list5))
+
+# list5.append(employee1)
+# print(list5)
+
+# tuples10 = (324,234,324,324,"adsfads","adsfa",employee1)
+# print(tuples10)
+# # tuples10.append(employee1)
+# print(type(tuples10))
+
+mylist = []
+print(mylist)
+print(type(mylist))
+
+mytuples = ()
+print(mytuples)
+print(type(mytuples))
+
+mydata = {}
+print(mydata)
+print(type(mydata))
+
+myset = set()
+print(myset)
+print(type(myset))
+
+myset.add(5)
+print(myset)
+
+mynewset = {4}
+print(type(mynewset))
+print(mynewset)
