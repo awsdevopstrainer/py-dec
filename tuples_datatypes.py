26ae85e61fb2f6ccd3f4ddf351b6e1aa1b5fd86d
awsdevopstrainer <69856631+awsdevopstrainer@users.noreply.github.com>
2025-12-23 08:02:42 +0530

    Create tuples_datatypes.py

---
+++ tuples_datatypes.py
@@ -0,0 +1,16 @@
+my_tuple1 = (1,2,3,4,5,6,34,5,6,3,2,2,3,4)
+
+print(my_tuple1)
+print(type(my_tuple1))
+
+print(my_tuple1[2:5])
+
+print(dir(my_tuple1))
+
+print(my_tuple1.count(3))
+
+for i in my_tuple1:
+    print(i)
+    
+mylist = list(my_tuple1)
+print(mylist)
