c465fe768061d787ae4b19215c0465ed5709d25c
awsdevopstrainer <69856631+awsdevopstrainer@users.noreply.github.com>
2026-02-06 07:51:50 +0530

    Create os-subproc-sys.py with os and subprocess examples
    Add os-subproc-sys.py with various os and subprocess examples.

---
+++ os-subproc-sys.py
@@ -0,0 +1,75 @@
+import os
+import subprocess
+
+import sys
+
+# print(dir(os))
+
+# print(os.name)
+# cwd = os.getcwd()
+# print(os.getcwd())
+
+# print(os.getpid())
+
+# print(os.listdir())
+
+# os.chdir("C://Users//santh//OneDrive//Documents//Zoom")
+
+# print(os.getcwd())
+
+# mytext = """lkajsdklfkalsdjfasdfaskdlfjklajsdf
+# a
+
+# sadfasdfjlka
+
+# asdfsa"""
+
+# f = open("newfile.config","w")
+
+# f.write(mytext)
+# f.close()
+
+# print(os.listdir())
+
+# # os.rmdir("New folder")
+
+# # os.remove("newfile.txt")
+
+# print(os.listdir())
+
+# # stats = os.stat("newfile.txt")
+
+# # print(dir(stats))
+
+# os.environ["FILESIZE"] = "100m"
+
+# print(os.getenv("FILESIZE"))
+
+# if not os.path.exists("newfile.config"):
+#     print("The file does not exist")
+
+# # print(os.getenv())
+
+# # os.chdir(cwd)
+
+# # print(os.getcwd())
+
+# res = subprocess.run(["ls","-l"],capture_output=True)
+
+# print(res.returncode)
+# print(res.stderr)
+# print(res.stdout)
+
+# print(sys.argv)
+# print(len(sys.argv))
+# if len(sys.argv) != 3:
+#     print("Please enter the name as first argument and email as second argument")
+
+# else:
+#     username = sys.argv[1]
+
+#     email = sys.argv[2]
+
+#     print(f"Hey {username}, welcome to the club. I will contact you on {email} this email")
+
+
